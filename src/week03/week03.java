fbf4f32a87b76dbc2250112a1cf215481b018f18
deepak-getpu <deepak.getpu@gmail.com>
2023-12-02 00:46:30 -0500

    new files added

---
+++ src/week03/week03.java
@@ -0,0 +1,11 @@
+package week001week001;
+
+public class Demoweek001week001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
